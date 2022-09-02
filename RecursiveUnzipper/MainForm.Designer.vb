4d6d95cea8a9591b218f5bda2f0fd92ae51fce1c
vijaysridhara <vsridhara@gmail.com>
2022-09-02 18:09:13 +0530

    Add project files.

---
+++ RecursiveUnzipper/MainForm.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtZip = New System.Windows.Forms.TextBox()
+        Me.butSelectFile = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.butSelectOutput = New System.Windows.Forms.Button()
+        Me.butUnzip = New System.Windows.Forms.Button()
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout
+        '
+        'txtZip
+        '
+        Me.txtZip.Location = New System.Drawing.Point(35, 18)
+        Me.txtZip.Name = "txtZip"
+        Me.txtZip.ReadOnly = true
+        Me.txtZip.Size = New System.Drawing.Size(268, 23)
+        Me.txtZip.TabIndex = 0
+        '
+        'butSelectFile
+        '
+        Me.butSelectFile.Location = New System.Drawing.Point(310, 13)
+        Me.butSelectFile.Name = "butSelectFile"
+        Me.butSelectFile.Size = New System.Drawing.Size(119, 31)
+        Me.butSelectFile.TabIndex = 1
+        Me.butSelectFile.Text = "Select zip file"
+        Me.butSelectFile.UseVisualStyleBackColor = true
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(35, 52)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = true
+        Me.txtOutput.Size = New System.Drawing.Size(268, 23)
+        Me.txtOutput.TabIndex = 2
+        '
+        'butSelectOutput
+        '
+        Me.butSelectOutput.Location = New System.Drawing.Point(310, 47)
+        Me.butSelectOutput.Name = "butSelectOutput"
+        Me.butSelectOutput.Size = New System.Drawing.Size(119, 31)
+        Me.butSelectOutput.TabIndex = 3
+        Me.butSelectOutput.Text = "Select output folder"
+        Me.butSelectOutput.UseVisualStyleBackColor = True
+        '
+        'butUnzip
+        '
+        Me.butUnzip.Location = New System.Drawing.Point(435, 44)
+        Me.butUnzip.Name = "butUnzip"
+        Me.butUnzip.Size = New System.Drawing.Size(119, 31)
+        Me.butUnzip.TabIndex = 4
+        Me.butUnzip.Text = "Unzip"
+        Me.butUnzip.UseVisualStyleBackColor = True
+        '
+        'txtLog
+        '
+        Me.txtLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtLog.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtLog.Location = New System.Drawing.Point(12, 84)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.ReadOnly = True
+        Me.txtLog.Size = New System.Drawing.Size(542, 300)
+        Me.txtLog.TabIndex = 5
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 396)
+        Me.Controls.Add(Me.butUnzip)
+        Me.Controls.Add(Me.butSelectOutput)
+        Me.Controls.Add(Me.butSelectFile)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtZip)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Recursize Unzipper"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents txtZip As TextBox
+    Friend WithEvents butSelectFile As Button
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents butSelectOutput As Button
+    Friend WithEvents butUnzip As Button
+    Friend WithEvents txtLog As TextBox
+End Class
---
+++ RecursiveUnzipper/MainForm.vb
@@ -0,0 +1,197 @@
+﻿Imports System.IO.Compression
+Imports SevenZipExtractor
+'***********************************************************************
+'Copyright 2022 Vijay Sridhara
+
+'Licensed under the Apache License, Version 2.0 (the "License");
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the License at
+
+'http://www.apache.org/licenses/LICENSE-2.0
+
+'Unless required by applicable law or agreed to in writing, software
+'distributed under the License is distributed on an "AS IS" BASIS,
+'WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'See the License for the specific language governing permissions and
+'limitations under the License.
+'***********************************************************************
+Public Class MainForm
+    Private unzipFolder As String
+    Dim selfile As String
+    Private Sub butUnzip_Click(sender As Object, e As EventArgs) Handles butUnzip.Click
+        unzipFolder = My.Computer.FileSystem.SpecialDirectories.Temp & "\temp00000unzip"
+        If IO.File.Exists(txtZip.Text) = False Then
+            LogMessage("The file doesn't exist: " & txtZip.Text)
+            Exit Sub
+        End If
+        If String.IsNullOrEmpty(txtZip.Text) Then
+            LogMessage("First select a file to unzip(zip, or rar)")
+            Exit Sub
+        End If
+        If String.IsNullOrEmpty(txtOutput.Text) Then
+            LogMessage("Output directory must be selected, for extracting...")
+            Exit Sub
+        End If
+        If Not IO.Directory.Exists(txtOutput.Text) Then
+            LogMessage("The selected output directory doesn't exist " & txtOutput.Text)
+            Exit Sub
+
+        End If
+        If Not IO.Directory.Exists(unzipFolder) Then
+            IO.Directory.CreateDirectory(unzipFolder)
+
+        End If
+        LogMessage("Started **************")
+        System.IO.Directory.Delete(unzipFolder, True)
+        Dim zippath As String = txtZip.Text
+        IO.Directory.CreateDirectory(unzipFolder)
+        Dim extrPath As String = txtOutput.Text
+        Try
+            If selfile = "zip" Then
+                ZipFile.ExtractToDirectory(zippath, unzipFolder)
+            Else
+                Dim unra As New SevenZipExtractor.ArchiveFile(zippath)
+                unra.Extract(unzipFolder, True)
+                unra.Dispose()
+            End If
+        Catch ex As Exception
+            LogMessage(ex.Message)
+            LogMessage("Stopped **************")
+            Exit Sub
+        End Try
+        CopyToFolder(unzipFolder, extrPath)
+        System.IO.Directory.Delete(unzipFolder, True)
+        LogMessage("Completed successfully **************")
+    End Sub
+    Private Sub LogMessage(msg As String)
+        txtLog.Text += msg & vbCrLf
+        txtLog.SelectionStart = txtLog.TextLength
+        txtLog.ScrollToCaret()
+        txtLog.Refresh()
+    End Sub
+    Private Sub CopyToFolder(tempFolder As String, outputFolder As String)
+        LogMessage("Checking " & tempFolder)
+        Try
+            Dim fls() As String = IO.Directory.GetFiles(tempFolder, "*.zip")
+
+            For Each f As String In fls
+                LogMessage("Found zip file " & f)
+                Try
+                    ZipFile.ExtractToDirectory(f, tempFolder)
+                Catch ex As Exception
+                    LogMessage("[Error in zip file " & f & "]")
+                    LogMessage(ex.Message)
+                End Try
+
+
+            Next
+            fls = IO.Directory.GetFiles(tempFolder, "*.rar")
+            For Each f As String In fls
+                LogMessage("Found rar file " & f)
+                Try
+                    Dim unra As New SevenZipExtractor.ArchiveFile(f)
+
+                    unra.Extract(outputFolder, True)
+                    unra.Dispose()
+                Catch ex As Exception
+                    LogMessage("[Error in rar file " & f & "]")
+                    LogMessage(ex.Message)
+                End Try
+
+            Next
+            fls = IO.Directory.GetFiles(tempFolder, "*.jpg")
+            LogMessage("Found " & fls.Length & " jpg files in " & tempFolder)
+
+            Try
+                If Not IO.Directory.Exists(outputFolder) Then
+                    IO.Directory.CreateDirectory(outputFolder)
+
+                End If
+            Catch ex As Exception
+                LogMessage(ex.Message)
+            End Try
+
+            For Each f As String In fls
+                Try
+
+                    IO.File.Copy(f, outputFolder & "\" & IO.Path.GetFileName(f))
+                Catch ex As Exception
+                    LogMessage(ex.Message)
+                End Try
+
+            Next
+            fls = IO.Directory.GetFiles(tempFolder, "*.png")
+            LogMessage("Found " & fls.Length & " png files in " & tempFolder)
+            For Each f As String In fls
+
+                Try
+
+                    IO.File.Copy(f, outputFolder & "\" & IO.Path.GetFileName(f))
+                Catch ex As Exception
+                    LogMessage(ex.Message)
+                End Try
+            Next
+            fls = IO.Directory.GetFiles(tempFolder, "*.bmp")
+            LogMessage("Found " & fls.Length & " bmp files in " & tempFolder)
+            For Each f As String In fls
+                If Not IO.Directory.Exists(outputFolder) Then
+                    IO.Directory.CreateDirectory(outputFolder)
+
+                End If
+                IO.File.Copy(f, outputFolder & "\" & IO.Path.GetFileName(f))
+            Next
+            fls = IO.Directory.GetFiles(tempFolder, "*.ico")
+            LogMessage("Found " & fls.Length & " ico files in " & tempFolder)
+            For Each f As String In fls
+                If Not IO.Directory.Exists(outputFolder) Then
+                    IO.Directory.CreateDirectory(outputFolder)
+
+                End If
+                IO.File.Copy(f, outputFolder & "\" & IO.Path.GetFileName(f))
+            Next
+            Dim dis() As String = IO.Directory.GetDirectories(tempFolder)
+            LogMessage("Found " & dis.Length & " directories in " & tempFolder)
+            For Each di As String In dis
+                If IO.Directory.Exists(outputFolder & "\" & IO.Path.GetFileName(di)) = False Then
+                    IO.Directory.CreateDirectory(outputFolder & "\" & IO.Path.GetFileName(di))
+                End If
+                CopyToFolder(di, outputFolder & "\" & IO.Path.GetFileName(di))
+            Next
+        Catch ex As Exception
+            LogMessage(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub butSelectFile_Click(sender As Object, e As EventArgs) Handles butSelectFile.Click
+        Dim selfdg As New OpenFileDialog
+        With selfdg
+            .InitialDirectory = My.Settings.ZipLocation
+            .Filter = "Zip files|*.zip|Rar files|*.rar"
+            If .ShowDialog = DialogResult.OK Then
+                txtZip.Text = .FileName
+                My.Settings.ZipLocation = IO.Path.GetInvalidFileNameChars(.FileName)
+                My.Settings.Save()
+            End If
+            If .FilterIndex = 0 Then
+                selfile = "zip"
+            Else
+                selfile = "rar"
+            End If
+        End With
+    End Sub
+
+    Private Sub butSelectOutput_Click(sender As Object, e As EventArgs) Handles butSelectOutput.Click
+        Dim ofd As New FolderBrowserDialog
+        With ofd
+            .InitialDirectory = My.Settings.OutputLocation
+            If .ShowDialog = DialogResult.OK Then
+                txtOutput.Text = .SelectedPath
+                My.Settings.OutputLocation = .SelectedPath
+                My.Settings.Save()
+            End If
+        End With
+
+    End Sub
+End Class
